4353b5da7d8e1a39159a64603bcb2ef24d4cf85e
wyang36 <kira.w.yang@gmail.com>
2024-02-08 16:13:43 -0800

    try make client side

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+'use client'
+
 import Image from "next/image";
 
 export default function Home() {
-  const seed = Math.floor(Math.random() * 100)
+  const seed = Math.ceil(Math.random() * 100)
   const filename = `/images/outputs/seed${seed.toString().padStart(4, '0')}.png`
   return (
     <div className="relative flex place-items-center">
